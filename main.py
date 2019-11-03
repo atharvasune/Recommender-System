f22b081b7083f1276e23996e79790676f305fb5b
PrakharGoenka <prakhar.goinka@gmail.com>
2019-11-03 14:25:42 +0530

    Restructure project directory.

---
+++ main.py
@@ -0,0 +1,22 @@
+# import all classes from recommenders directory
+
+def main():
+
+	'''
+		Initialize objects for all classes (or maybe make static methods);
+		and call functions to
+		(1) get recommendations
+		(2) calculate RMSE
+		(3) calculate MAE
+		(4) execution time
+		for each of the recommender types
+	'''
+
+
+
+
+
+
+# CHECK THIS PART!!
+if __name__ == '__main__':
+    main()
---
+++ recommenders/svd.py
@@ -0,0 +1,7 @@
+# module for svd
+
+class Svd():
+
+
+if __name__ == '__main__':
+	pass
